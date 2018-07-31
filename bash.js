065f8ba67a99ff5be7e7a5b1f673365914d6377d
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-07-31 14:54:50 -0400

    Finished pwd function and ls function

--- bash.js
+++ bash.js
@@ -1,13 +1,18 @@
 const pwd = require('./pwd').pwdFunc;
+const ls = require('./ls').ls;
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    pwd();
+    process.stdout.write(pwd());
   }
 
-  // process.stdout.write(cmd);
+  if (cmd === 'ls') {
+    ls();
+  }
+
+
   process.stdout.write('\nprompt > ');
 });
